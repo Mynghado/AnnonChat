f303bfd6e51d1d78903c75eadf09011562baafb7
Mynghado <kaamec@gmail.com>
2019-01-12 20:04:04 +0100

    removed useless comms

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,4 @@
 const functions = require('firebase-functions');
-//const admin = require('firebase-admin');
 const cors = require('cors');
 const express = require('express');
 const app = express();
@@ -14,8 +13,6 @@ app.use(cors({ origin: true }));
 //  response.send("Hello from Firebase!");
 // });
 
-//admin.initializeApp()
-
 /*exports.checkAlias = functions.https.onCall((data, context) => {
     const ref = admin.firestore().collection('messages').doc(data.name);
     return ref.get().then(doc => {
@@ -26,38 +23,7 @@ app.use(cors({ origin: true }));
 
 })*/
 
-/*exports.test = functions.https.onRequest((req, res) => {
-    if (req.method == 'POST') {
-        return res.status(500).json({
-            message: 'It cannot work'
-        });
-    }
-    res.status(200).json({
-        message: 'It worked!'
-    });
-});
-
-app.get('/num', (req, res) => {
-    res.status(200).json({
-        message: 'Hello' // address + /app + /home
-    });
-});
-
-exports.app = functions.https.onRequest(app);*/
-
-// build multiple CRUD interfaces:
-//app.get('/', (req, res) => res.send({ name: 'Yoshi' }));
-
-/*app.post('/', (req, res) => res.send(Widgets.create()));
-app.put('/:id', (req, res) => res.send(Widgets.update(req.params.id, req.body)));
-app.delete('/:id', (req, res) => res.send(Widgets.delete(req.params.id)));
-app.get('/', (req, res) => res.send(Widgets.list()));*/
-
-// Expose Express API as a single Cloud Function:
-//exports.widgets = functions.https.onRequest(app);
-
 exports.chat = functions.https.onRequest((req, res) => {
-
     app.use('/', require('./routes/chat.route'));
 
     // to handle requests with "/(function)" without the last '/'
